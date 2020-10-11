b9e09cddb33a8012bc5a9ae426a8f6336a56623a
Aleksey Kayeshko <kayeshko@adani.by>
2020-10-11 12:47:31 +0300

    Add project files.

---
+++ Ostis.ExtensionNetAdapter/NativeAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Ostis.ExtensionNetAdapter
+{
+    public static class NativeAdapter
+    {
+        [DllExport]
+        public static int sc_module_initialize()
+        {
+            File.Create("1.txt");
+            Console.WriteLine("!!!!! I did it!!!!");
+            return 1;
+        }
+
+        [DllExport]
+        public static int sc_module_shutdown()
+        {
+            File.Create("2.txt");
+            return 1;
+        }
+    }
+}
