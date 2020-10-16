05797863a1245f78e589959d2caa887a7aa42d55
Aleksey Kayeshko <kayeshko@adani.by>
2020-10-16 09:36:52 +0300

    [+] Add linux project

---
+++ libnet-ext/NativeAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Ostis.ExtensionNetAdapter
+{
+    public static class NativeAdapter
+    {
+        [UnmanagedCallersOnlyAttribute(EntryPoint = "sc_module_initialize")]
+        public static int sc_module_initialize()
+        {
+            File.Create("1.txt");
+            Console.WriteLine("!!!!! I did it!!!!");
+            return 1;
+        }
+
+        [UnmanagedCallersOnlyAttribute(EntryPoint = "sc_module_shutdown")]
+        public static int sc_module_shutdown()
+        {
+            File.Create("2.txt");
+            return 1;
+        }
+    }
+}
